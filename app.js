2e030b0805d80fc466b7cde9d0bbda9515293ad5
Mariola Moreno <mariolamoreno@hotmail.es>
2017-03-28 15:15:27 +0200

    cambiando puerto

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ function errorHandler(err, req, res, next) {
 		app.use(function(req, res){
 			res.sendStatus(404);
 		});
-		var server = app.listen(8080, function() {
+		var server = app.listen(80, function() {
 			var port = server.address().port;
 			console.log('Express server escuchando en %s.', port);
 		});
